e3c69edcfce218fdf9e3f0252eca250d61612d3b
bkadukun <balaraju_kadukuntla@mentor.com>
2021-08-30 02:26:31 +0530

    Project reporting service
    Unit tests

--- src/project/ProjectMain.java
+++ src/project/ProjectMain.java
@@ -30,7 +30,8 @@ public class ProjectMain {
                 "1223456,2345,us_west,BlueTeam,ProjectBanana,2211s\n" +
                 "3244332,2346,eu_west,YellowTeam3,ProjectCarrot,4322s\n" +
                 "1233456,2345,us_west,BlueTeam,ProjectDate,2221s\n" +
-                "3244132,2346,eu_west,YellowTeam3,ProjectEgg,4122s";
+                "3244132,2346,eu_west,YellowTeam3,ProjectEgg,4122s\n" +
+                "3244332,2346,us_west,BlueTeam,ProjectBanana,2211s";
     }
 
     private static IProject createProject(ProjectFactory projectFactory, String projectData) {
